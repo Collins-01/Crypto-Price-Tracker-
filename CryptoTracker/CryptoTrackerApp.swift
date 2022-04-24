5312e654d00727cc8db24ca90347c50222638902
Collins Oriakhi <collins.oriakhi@itara.co>
2022-04-24 11:54:05 +0100

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Oriakhi Collins on 4/24/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
