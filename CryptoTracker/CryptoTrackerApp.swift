683f1b566bd8c33d5c7cac25dac115df686489ae
Collins Oriakhi <collins.oriakhi@itara.co>
2022-06-30 22:32:11 +0100

    Added Launch View

--- CryptoTracker/CryptoTrackerApp.swift
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -10,19 +10,30 @@ import SwiftUI
 @main
 struct CryptoTrackerApp: App {
     @StateObject private var viewModel = HomeScreenViewModel()
+    @State private var showLaunchView : Bool = true
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
     }
     var body: some Scene {
         WindowGroup {
-            NavigationView{
-                HomeScreen()
-                    .navigationBarHidden(true)
+            ZStack {
+                NavigationView{
+                    HomeScreen()
+                        .navigationBarHidden(true)
+                    
+                }
+                .environmentObject(viewModel)
                 
+                
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0)
             }
-            .environmentObject(viewModel)
-//            LoginScreen()
             
         }
         
